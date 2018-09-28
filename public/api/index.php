01b1a44082ba92cb6c2fa742f41aec423cd0d37a
george-tr6 <george.tr6@gmail.com>
2018-09-28 12:34:51 -0700

    public folder

---
+++ public/api/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$output = [
+    "success" => true,
+    "message" => 'The API is working'
+];
+
+$output =json_encode($output);
+
+echo $output;
---
+++ src/components/test.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+    render(){
+        return <h1>API Test Component</h1>
+    }
+}
+
+export default Test;
